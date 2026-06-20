917c30f84533a1b0bbd669fd67276c23ee287332
Bam123483 <bamthiti@gmail.com>
2026-06-20 13:40:43 +0700

    Update Web.py

--- Web.py
+++ Web.py
@@ -1,32 +1,7 @@
 import streamlit as st
 from ultralytics import YOLO
+from PIL import Image
 import tempfile
-
-st.set_page_config(
-    page_title="Tire Damage Detector",
-    layout="centered"
-)
-
-st.markdown("""
-<style>
-.stApp {
-    background-color: #000000;
-}
-
-h1 {
-    color: #ff8800;
-    text-align: center;
-}
-
-[data-testid="stFileUploader"] {
-    background-color: #111111;
-    border: 2px solid #ff8800;
-    border-radius: 10px;
-    padding: 10px;
-}
-
-</style>
-""", unsafe_allow_html=True)
 
 @st.cache_resource
 def load_model():
@@ -34,10 +9,10 @@ def load_model():
 
 model = load_model()
 
-st.title("🚗 Tire Damage Detector")
+st.set_page_config(page_title="Tire Damage Detector")
 
 uploaded_file = st.file_uploader(
-    "Upload a Tire Image",
+    "",
     type=["jpg", "jpeg", "png"]
 )
 
@@ -51,13 +26,11 @@ if uploaded_file is not None:
         tmp.write(uploaded_file.getvalue())
         temp_path = tmp.name
 
-    with st.spinner("Detecting..."):
+    results = model(temp_path, conf=0.10)
 
-        results = model(temp_path, conf=0.10)
+    plotted = results[0].plot()
 
-        plotted = results[0].plot()
-
-        st.image(
-            plotted,
-            use_container_width=True
-        )
+    st.image(
+        plotted,
+        use_container_width=True
+    )
