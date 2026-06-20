ddd993d15e080cbc20c4ea35fc587b805049529c
Bam123483 <bamthiti@gmail.com>
2026-06-20 13:38:21 +0700

    Update Web.py

--- Web.py
+++ Web.py
@@ -9,47 +9,28 @@ def load_model():
 
 model = load_model()
 
-st.title("🚗 Tire Damage Detector")
+st.set_page_config(page_title="Tire Damage Detector")
 
 uploaded_file = st.file_uploader(
-    "Upload a tire image",
+    "",
     type=["jpg", "jpeg", "png"]
 )
 
-# AUTO DETECT
 if uploaded_file is not None:
 
-    image = Image.open(uploaded_file)
+    with tempfile.NamedTemporaryFile(
+        delete=False,
+        suffix=".jpg"
+    ) as tmp:
+
+        tmp.write(uploaded_file.getvalue())
+        temp_path = tmp.name
+
+    results = model(temp_path, conf=0.10)
+
+    plotted = results[0].plot()
 
     st.image(
-        image,
-        caption="Uploaded Image",
+        plotted,
         use_container_width=True
     )
-
-    with st.spinner("Detecting..."):
-
-        with tempfile.NamedTemporaryFile(
-            delete=False,
-            suffix=".jpg"
-        ) as tmp:
-
-            tmp.write(uploaded_file.getvalue())
-            temp_path = tmp.name
-
-        results = model(temp_path, conf=0.10)
-
-        has_detection = len(results[0].boxes) > 0
-
-        if has_detection:
-            st.error("⚠️ Damage detected on tire")
-        else:
-            st.success("✅ No problems detected. Tire looks good.")
-
-        plotted = results[0].plot()
-
-        st.image(
-            plotted,
-            caption="Detection Result",
-            use_container_width=True
-        )
